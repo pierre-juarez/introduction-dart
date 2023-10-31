00049984679630baa89f75d43be36a74b6a2ff43
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2023-10-31 00:57:22 -0500

    [add] - Hello world

---
+++ hello.dart
@@ -0,0 +1,8 @@
+void main() {
+  var myName = 'Pierre';
+  // const myName = 'Pierre';
+  // late final myName;
+  // final myName;
+  print("Hello $myName");
+  print("Hello ${myName.toUpperCase()}");
+}
