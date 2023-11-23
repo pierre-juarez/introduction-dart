9a61fb9e0b1e3bac44efb9dcf2e04c6500120da9
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2023-11-23 09:28:05 -0500

    [add] - Abstract class

---
+++ 09_abstract_class.dart
@@ -0,0 +1,17 @@
+void main() {
+  // final windPlant = EnergyPlant();
+}
+
+enum PlantType { nuclear, wind, water }
+
+abstract class EnergyPlant {
+  double energyLeft;
+  PlantType type;
+
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type,
+  });
+
+  void consumeEnergy(double amount);
+}
